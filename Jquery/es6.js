cae1c173bb28b237b4c7fac1fc01adbead786559
Chouyitzeng001 <ivan59438@gmail.com>
2022-12-21 20:21:26 +0800

    add

---
+++ Jquery/es6.js
@@ -0,0 +1,18 @@
+    var i = 110;
+    let j = 80;
+    const PI = 3.14159;
+
+{
+    var i = 115;
+    let j = 85;
+    const PI = 3.14;
+
+    console.log("i=" +i);
+    console.log("j=" +j);
+    console.log("PI=" +PI);
+}
+
+
+    console.log("i=" +i);
+    console.log("j=" +j);
+    console.log("PI=" +PI);
